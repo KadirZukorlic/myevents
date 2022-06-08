49058a83db843d23f1b19b91a86fa7e4a09504b5
unknown <kadirzukorlic@gmail.com>
2022-06-08 12:31:18 +0200

    styles and icons added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from 'next/link';
+import { Button } from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import classes from './event-item.module.css';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 export const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -13,17 +17,28 @@ export const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <time>{humanReadableDate}</time>
-      </div>
-      <div>
-        <address>{formattedAddress}</address>
-      </div>
-      <div>
-        <Link href={exploreLink}>Explore Event</Link>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+        </div>
+        <div className={classes.address}>
+          <AddressIcon />
+          <address>{formattedAddress}</address>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
       </div>
     </li>
   );
