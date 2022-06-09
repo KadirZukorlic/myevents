32da755b67b4036bb51b185b1e86858f08f3a647
unknown <kadirzukorlic@gmail.com>
2022-06-10 00:47:10 +0200

    events search component added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,15 @@
+import { getAllEvents } from '../../dummy-data';
+import { EventList } from '../../components/events/event-list';
+import { EventsSearch } from '../../components/events/events-search';
+import { Fragment } from 'react';
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All events</h1>
+      <EventsSearch />
+      <EventList items={events} />
     </div>
   );
 };
