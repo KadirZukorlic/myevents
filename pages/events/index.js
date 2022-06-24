1ac24719ccc29ca5a3b6b22fa4d0ab834035fdc2
unknown <kadirzukorlic@gmail.com>
2022-06-24 17:59:42 +0200

    slug page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import { Fragment } from 'react';
-import { useRouter, userRouter } from 'next/router';
-import { getAllEvents } from '../../helpers';
+import { useRouter } from 'next/router';
+import { getAllEvents } from '../../helpers/api-util';
 import { EventList } from '../../components/events/event-list';
 import { EventsSearch } from '../../components/events/events-search';
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = (props) => {
+  const { events } = props;
   const router = useRouter();
 
   const findEventsHandler = (year, month) => {
@@ -20,6 +20,17 @@ const AllEventsPage = () => {
       <EventList items={events} />
     </Fragment>
   );
+};
+
+export const getStaticProps = async () => {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60
+  };
 };
 
 export default AllEventsPage;
