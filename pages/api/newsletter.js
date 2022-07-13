b69585123d0bc8fe22cf099f0f7504567fdd004f
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-07-13 18:06:58 +0200

    connected to the mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,24 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+const CONNECT_URL =
+  'mongodb+srv://Kadir:starfallkacobre1@cluster0.h22nx.mongodb.net/?retryWrites=true&w=majority';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
-    //short validation
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(CONNECT_URL);
+    const db = client.db('newsletter');
+
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up! Your email is: ' + userEmail });
   }
 }
