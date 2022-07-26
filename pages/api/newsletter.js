e325b503f5383a3ce41e2cbb7b4712468bb39200
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-07-27 01:53:51 +0200

    error handling in newsletter route

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -3,6 +3,16 @@ import { MongoClient } from 'mongodb';
 const CONNECT_URL =
   'mongodb+srv://Kadir:starfallkacobre1@cluster0.h22nx.mongodb.net/?retryWrites=true&w=majority';
 
+const connectDatabase = async () => {
+  const client = await MongoClient.connect(CONNECT_URL);
+  return client;
+};
+
+const insertDocument = async (client, document) => {
+  const db = client.db('newsletter');
+  await db.collection('emails').insertOne(document);
+};
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -12,16 +22,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(CONNECT_URL);
-    const db = client.db('newsletter');
+    let client;
 
-    const result = await db
-      .collection('emails')
-      .insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
 
-    // console.log(result);
-
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' });
+      return;
+    }
 
     res.status(201).json({ message: 'Signed up! Your email is: ' + userEmail });
   }
